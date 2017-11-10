1b30088ecc73765873ed1efbfac6b72688c88d02
Glyne Gittens <jgit121@gmail.com>
2017-11-10 15:35:15 -0500

    updates to account for missing dependencies

--- minecraft.sh
+++ minecraft.sh
@@ -4,6 +4,11 @@ PORT=25565
 URL="https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar"
 MAX="512M"
 MIN="512M"
+DEB="deb http://ftp.us.debian.org/debian/ jessie main contrib non-free
+deb http://security.debian.org/ jessie/updates main contrib non-free
+deb http://ftp.us.debian.org/debian/ jessie-updates main contrib non-free
+deb http://ftp.us.debian.org/debian/ jessie-backports main contrib non-free"
+
 while [ "$#" -gt 0 ] ; do 
     case $1 in
         -p)
@@ -26,6 +31,16 @@ while [ "$#" -gt 0 ] ; do
 done
 echo $PORT
 
+# Add backport dependencies for openjdk-8-jre
+if [ -f /etc/apt/openjdk8.list ] ; then
+    echo "JDK 8 Installed"
+else
+    sudo touch /etc/apt/openjdk8.list  
+    sudo chmod 755 /etc/apt/openjdk8.list 
+    cat $DEB > /etc/apt/openjdk8.list
+fi
+
+# Install Java if it doesn't already exist on this machine
 sudo apt -y install openjdk-8-jre-headless
 if [ -f minecraft_server*.jar ] ; then 
     echo "skipping download"
@@ -33,6 +48,7 @@ else
     wget $URL
 fi
 
+# If the eula.txt already exists then skip intial setup
 if [ -f eula.txt ] ; then 
     echo "Dont run initial java setup"
 else
@@ -40,6 +56,7 @@ else
     java -Xmx$MAX -Xms$MIN -jar minecraft_server*.jar nogui
 fi
 
+# Allow connections on the port
 sudo ufw allow $PORT
 sed -i "s/eula=false/eula=true/" ./eula.txt
 java -Xmx$MAX -Xms$MIN -jar minecraft_server*.jar nogui
